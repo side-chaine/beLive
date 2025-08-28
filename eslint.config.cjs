2b909b70b95eec9e0a6642b137c2e8879d879f7d
Nikita Side-Chaine <nikitosss007@gmail.com>
2025-08-29 02:48:30 +0300

    chore: migrate ESLint config to Flat Config system

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,25 +1,33 @@
-module.exports = {
-  ignores: [
-    "Karaoke/**",
-    "Rehearsal/**",
-    "img/**",
-    "js/vendor/**",
-    "css/**"
-  ],
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { args: "none", varsIgnorePattern: "^_" }],
+      "no-console": "off",
+      eqeqeq: "warn",
+      curly: "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "Karaoke/**",
+      "Rehearsal/**",
+      "img/**",
+      "js/vendor/**",
+      "css/**"
+    ],
   },
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended"],
-  rules: {
-    "no-unused-vars": ["warn", { "args": "none", "varsIgnorePattern": "^_" }],
-    "no-console": "off",
-    "eqeqeq": "warn",
-    "curly": "warn"
-  }
-};
+];
